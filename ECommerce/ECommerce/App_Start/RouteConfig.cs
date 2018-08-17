aab7d4bb1a32d917b3a4ad820b5a7b1c3ea1e75e
Aluno <Aluno@DOSO121743>
2018-08-17 08:40:06 -0300

    Alterando Layout

--- ECommerce/ECommerce/App_Start/RouteConfig.cs
+++ ECommerce/ECommerce/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace ECommerce
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Produto", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ProdutoHome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
