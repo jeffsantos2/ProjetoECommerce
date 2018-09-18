03e173e84db9f30c5591c8963cc7b93be70d5242
Jefferson <jeffersondossantos1995@hotmail.com>
2018-09-18 11:44:34 -0300

    Api

---
+++ ECommerce/ECommerce/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace ECommerce
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            //mudar o formato da entrega dos dados da webapi
+            //de xml para json
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.Indent = true;
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ECommerce/ECommerce/Controllers/ProdutoAPIController.cs
@@ -0,0 +1,63 @@
+﻿using ECommerce.DAL;
+using ECommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ECommerce.Controllers
+{
+    [RoutePrefix("api/Produto")]
+    public class ProdutoAPIController : ApiController
+    {
+        static ProdutoDAO produtoDAO = new ProdutoDAO();
+        [Route("Produtos")]
+        public List<Produto> GetProdutos()
+        {
+            return produtoDAO.ListarProdutos();
+        }
+
+        [Route("ProdutosPorCategoria/{CategoriaID}")]
+        public List<Produto> GetProdutosPorCategoria(int CategoriaID)
+        {
+            return produtoDAO.ListarProdutosPorCategoria(CategoriaID);
+        }
+
+        [Route("ProdutosPorID/{ProdutoID}")]
+        public dynamic GetProdutosPorID(int ProdutoID)
+        {
+            Produto produto =  produtoDAO.BuscarPorID(ProdutoID);
+            if(produto != null)
+            {
+                dynamic ProdutoDinamico = new
+                {
+                    Nome = produto.Nome,
+                    Preco = produto.Preco.ToString("C2"),
+                    Categoria = produto.Categoria.Nome,
+                    DataEnvio = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")
+                };
+                return ProdutoDinamico;
+            }
+            return NotFound();
+        }
+
+        [Route("CadastrarProduto")]
+        public IHttpActionResult PostCadastrarProduto(Produto produto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if(produtoDAO.Adicionar(produto))
+            {
+                return Created("", produto);
+            }
+            else
+            {
+                return Conflict();
+            }
+        }
+    }
+}
